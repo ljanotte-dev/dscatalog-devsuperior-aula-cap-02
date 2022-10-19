23bacab414b719ecc1b21fc2e226326214c2ea56
ljanotte-dev <ljanotte-dev@gmail.com>
2022-10-18 21:35:30 -0300

    DSCatalog Cap1

---
+++ backend/src/test/java/com/devsuperior/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
